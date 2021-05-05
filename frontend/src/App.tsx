88c5718c22c336792ce1e8ce9eb2a31f3f11d7d3
Mariotti07 <midiasmart99@gmail.com>
2021-05-05 17:49:04 -0300

    feat: Basic Status Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-
+import NavBar from "./components/NavBar";
+import React from "react";
+import Footer from "./components/Footer";
+import DataTable from "./components/DataTable";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">React is On</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">React is On</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 

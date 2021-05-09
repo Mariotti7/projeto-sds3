a9bdce665e24f059857b9464fb7ccc1bbd689555
Mariotti07 <midiasmart99@gmail.com>
2021-05-09 18:41:20 -0300

    feat: Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,10 +1,13 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import React, { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/requests";
 
 const DataTable = () => {
+
+  const [ activePage, setActivePage ] = useState(0) // const para alterar o numero da página
   const [page, setPage] = useState<SalePage>({
     first: true,
     last: true,
@@ -15,14 +18,19 @@ const DataTable = () => {
 
   useEffect(() => {
     axios
-      .get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+      .get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
       .then((response) => {
         setPage(response.data);
       });
-  }, []);
+  }, [activePage]);
+
+  const changePage = (index:number) =>{ // função para alterar o número da página
+    setActivePage(index)
+  }
 
   return (
     <>
+      <Pagination page={page} onPageChange={changePage} />
       <div className="table-responsive">
         <table className="table table-striped table-sm">
           <thead>
